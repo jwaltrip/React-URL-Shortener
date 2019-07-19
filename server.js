d6a7b0567f623fd85617531032138cd8dbd10cc7
Jake Waltrip <jake.waltrip@gmail.com>
2019-07-19 18:37:14 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config()
+const express = require("express")
+const bodyParser = require("body-parser")
+const logger = require("morgan")
+
+const app = express()
+
+// setup connection to sql database
+
+// import routes
+
+// setup middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(logger('dev'))
+
+// setup routes
+app.get("/", (req, res) => {
+  return res.json({ message: ".'. Hello Connected World .'." })
+})
+
+// set PORT var
+const port = process.env.PORT || 5000
+
+// setup app to listen on port
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
+
+module.exports = app
